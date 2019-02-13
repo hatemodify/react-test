b489a3ae0ec6c3a60366dbb5514d0093c8f15585
hatemodify <hatemodify@gmail.com>
2019-02-13 18:58:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
   render () {
-    return (
-      <div>
-        <input type='text' value='asd' />
-      </div>
-    )
+    const a = data => {
+      axios
+        .get('http://127.0.0.1:9998/near/37.4005651/127.10944169999999')
+        .then(res => {
+          const a = res.data
+
+          console.log(this)
+        })
+    }
+    a()
+
+    return <div>aaaaaa</div>
   }
 }
 
